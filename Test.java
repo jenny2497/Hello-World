6678000a9b9aaa170f1f6a30fed69a47c28d5808
jenny2497 <79676021+jenny2497@users.noreply.github.com>
2021-10-20 13:54:51 -0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String args[]){
+        
+          System.out.println("This is test for git fetch and pull command.");
+          return 0;
+    }
+}
